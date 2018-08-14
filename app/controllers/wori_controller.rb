26926870e85b28d3c8e4b7df5ecaa4dc03094cbd
DongGeol Kim <ehd6481@likelion.org>
2018-08-14 05:04:41 +0000

    first commit

---
+++ app/controllers/wori_controller.rb
@@ -0,0 +1,2 @@
+class WoriController < ApplicationController
+end
---
+++ test/controllers/wori_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WoriControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
